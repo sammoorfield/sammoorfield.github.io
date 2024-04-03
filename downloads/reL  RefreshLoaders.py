dd10713dea1cf638bf1b337d892ae6d1557cd3a2
sammoorfield <45549860+sammoorfield@users.noreply.github.com>
2024-04-03 17:26:06 +1100

    Update

--- downloads/reL  RefreshLoaders.py
+++
@@ -1,9 +0,0 @@
-#reL RefreshLoaders v0.1
-
-varToollist = composition.GetToolList(False, 'Loader')
-varToolSelected = composition.GetToolList(True, 'Loader')
-
-for tool in varToolSelected.values():
-	tool.Clip[composition.CurrentTime] = tool.Clip[composition.CurrentTime]
-
-
--- js/magnific-popup-options.js
+++ js/magnific-popup-options.js
@@ -1,110 +1,93 @@
 $(document).ready(function() {
-  // MagnificPopup
-  var magnifPopup = function() {
-    $('.image-popup').magnificPopup({
-      type: 'image',
-	  titleSrc: 'title',
-      removalDelay: 300,
-      mainClass: 'mfp-with-zoom',
-	  closeOnContentClick: true,
-	  closeOnBgClick: true,
-      gallery: {enabled: true, navigateByImgClick: true,},
-	  fixedBgPos: false,
-	  
+	// CGI
+	var magnifPopupCGI = function() {
+		$('.image-popup-cgi').magnificPopup({
+			type: 'image',
+			//titleSrc: 'title',
+			removalDelay: 300,
+			mainClass: 'mfp-with-zoom',
+			closeOnContentClick: true,
+			closeOnBgClick: true,
+			gallery: {enabled: true, navigateByImgClick: true,},
+			fixedBgPos: true,
+			fixedContentPos: false,
 
-	  
 
-      zoom: {
-        enabled: true, // By default it's false, so don't forget to enable it
-		
-        duration: 300, // duration of the effect, in milliseconds
-        easing: 'ease-in-out', // CSS transition easing function
-		
-        // The "opener" function should return the element from which popup will be zoomed in
-        // and to which popup will be scaled down
-        // By defailt it looks for an image tag:
-        opener: function(openerElement) {
-        // openerElement is the element on which popup was initialized, in this case its <a> tag
-        // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-        return openerElement.is('img') ? openerElement : openerElement.find('img');
-        }
-      }
-    });
-  };
-  
-  var RNDPopup = function() {
-    $('.rnd-popup').magnificPopup({
-      type: 'image',
-	  titleSrc: 'title',
-      removalDelay: 300,
-      mainClass: 'mfp-with-zoom',
-	  closeOnContentClick: true,
-	  closeOnBgClick: true,
-      gallery: {enabled: true, navigateByImgClick: true,},
-	  fixedBgPos: false,
-	  
+		image: {
+			titleSrc: function(item) {
+			var markup = '';
+			if (item.el[0].hasAttribute("data-title")) {
+			markup += item.el.attr('data-title');
+			}
+			return markup
+			}
+		},
 
-	  
+		zoom: {
+			enabled: true, // By default it's false, so don't forget to enable it
 
-      zoom: {
-        enabled: true, // By default it's false, so don't forget to enable it
-		
-        duration: 300, // duration of the effect, in milliseconds
-        easing: 'ease-in-out', // CSS transition easing function
-		
-        // The "opener" function should return the element from which popup will be zoomed in
-        // and to which popup will be scaled down
-        // By defailt it looks for an image tag:
-        opener: function(openerElement) {
-        // openerElement is the element on which popup was initialized, in this case its <a> tag
-        // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-        return openerElement.is('img') ? openerElement : openerElement.find('img');
-        }
-      }
-    });
-  };
-  
+			duration: 300, // duration of the effect, in milliseconds
+			easing: 'ease-in-out', // CSS transition easing function
 
-  
-  var showreelPopup = function() {
-    $('.popup-youtube').magnificPopup({
-        disableOn: 700,
-        type: 'iframe',
-        mainClass: 'mfp-fade',
-        removalDelay: 160,
-        preloader: false,
+			// The "opener" function should return the element from which popup will be zoomed in
+			// and to which popup will be scaled down
+			// By defailt it looks for an image tag:
+			opener: function(openerElement) {
+			// openerElement is the element on which popup was initialized, in this case its <a> tag
+			// you don't need to add "opener" option if this code matches your needs, it's defailt one.
+			return openerElement.is('img') ? openerElement : openerElement.find('img');
+			}
+			},
 
-        fixedContentPos: false,
-		
-		iframe: {
-        patterns: {
-            youtube: {
-                index: 'youtube.com/', 
-                id: function(url) {        
-                    var m = url.match(/[\\?\\&]v=([^\\?\\&]+)/);
-                    if ( !m || !m[1] ) return null;
-                    return m[1];
-                },
-                src: '//www.youtube.com/embed/%id%?autoplay=1'
-            },
-        }
-		}
-		
-		
-		
-		
-    });
-};
-  
-  
-  
-  
+		});
+	};
+	
+	// VFX
+	var magnifPopupVFX = function() {
+		$('.image-popup-vfx').magnificPopup({
+			type: 'image',
+			//titleSrc: 'title',
+			removalDelay: 300,
+			mainClass: 'mfp-with-zoom',
+			closeOnContentClick: true,
+			closeOnBgClick: true,
+			gallery: {enabled: true, navigateByImgClick: true,},
+			fixedBgPos: true,
+			fixedContentPos: false,
+
+
+		image: {
+			titleSrc: function(item) {
+			var markup = '';
+			if (item.el[0].hasAttribute("data-title")) {
+			markup += item.el.attr('data-title');
+			}
+			return markup
+			}
+		},
+
+		zoom: {
+			enabled: true, // By default it's false, so don't forget to enable it
+
+			duration: 300, // duration of the effect, in milliseconds
+			easing: 'ease-in-out', // CSS transition easing function
+
+			// The "opener" function should return the element from which popup will be zoomed in
+			// and to which popup will be scaled down
+			// By defailt it looks for an image tag:
+			opener: function(openerElement) {
+			// openerElement is the element on which popup was initialized, in this case its <a> tag
+			// you don't need to add "opener" option if this code matches your needs, it's defailt one.
+			return openerElement.is('img') ? openerElement : openerElement.find('img');
+			}
+			},
+
+		});
+	};
   
 
   
   // Call the functions 
-  showreelPopup();
-  magnifPopup();
-  RNDPopup();
-
+  magnifPopupCGI();
+  magnifPopupVFX();
 });
